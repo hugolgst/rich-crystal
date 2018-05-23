7d7c530854dd556d4b0e00d19a6df28a0fa15e80
AnanaGame <gameanana@gmail.com>
2018-05-23 22:02:43 +0200

    Create the activity method for using cmd: SET_ACTIVITY of rpc

--- src/rich-crystal/client.cr
+++ src/rich-crystal/client.cr
@@ -10,16 +10,51 @@ module RichCrystal
 
     # Log the Rich-crystal client by sending a first handshake
     def login
-      # Generate a JSON payload with RPC version: 1 and the client id
+      # Generate the handshake JSON payload
       payload = JSON.build do |json|
         json.object do
+          # Discord RPC setted to 1
           json.field "v", 1
           json.field "client_id", @client_id.to_s
+          # Set the current timestamp
+          json.field "nonce", Time.now.to_s("%s")
         end
       end
 
       # Send the handshake
       @ipc.send(RichCrystal::Ipc::Opcode::Handshake, payload)
+    end
+
+    # Set the rich presence activity with activity arugments
+    def activity(**activity)
+      # Generate the frame JSON payload
+      payload = JSON.build do |json|
+        json.object do
+          # Set cmd as SET_ACTIVITY for set Rich Presence activity
+          json.field "cmd", "SET_ACTIVITY"
+          json.field "args" do
+            payload_args(json, activity)
+          end
+          # Set the current timestamp
+          json.field "nonce", Time.now.to_s("%s")
+        end
+      end
+      @ipc.send(RichCrystal::Ipc::Opcode::Frame, payload)
+    end
+
+    # Create activity arguments as JSON
+    private def payload_args(json, activity : NamedTuple)
+      json.object do
+        # Set the process pid
+        json.field "pid", Process.pid
+        # Here are the activity arguments
+        json.field "activity" do
+          json.object do
+            json.field "state", activity[:state] unless activity[:state].nil?
+            json.field "details", activity[:details] unless activity[:details].nil?
+          end
+        end
+      end
     end
   end
 end
